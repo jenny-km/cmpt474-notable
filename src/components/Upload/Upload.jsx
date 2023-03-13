853dae5d0b9d25c2eeb0ce0eff887b5cf671d415
Jennifer Kim <jennykim029@gmail.com>
2023-03-12 20:38:33 -0700

    Upload Page

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -4,13 +4,13 @@ import Navbar from '../Navbar/Navbar';
 
 
 export const Upload = (props) => {
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
+    const[title, setTitle] = useState('');
+    const[university, setUniversity] = useState('');
+    const[course, setCourse] = useState('');
+    const[selectedFile, setSelectedFile] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
-
     }
     return(
         // body
@@ -18,16 +18,41 @@ export const Upload = (props) => {
             {/* top bar */}
             <Navbar/>
             <div>
-                <form className="login-form" onSubmit={handleSubmit}>
-                    <input value={email} onChange={(e)=> setEmail(e.target.value)} type="email" placeholder="Email" id="email" className="email"/>
-                    <br/>
-                    <input value={password} onChange={(e)=> setPassword(e.target.value)} type="password" placeholder="Password"/>
-                    <br/>
-                    <button type="submit">Log In</button>
+                <form className="upload-form" onSubmit={handleSubmit}>
+                    <div className="flex-container">
+                    <div className="flex-child">
+                        <label className="upload-labels" for="title">Document Title:</label>
+                        <br/>
+                        <input value={title} onChange={(e)=> setTitle(e.target.value)} type="text" id="title" className="upload-input"/>
+                        <br/>
+                        <br/>
+                        <label className="upload-labels" for="university">University:</label>
+                        <br/>
+                        <input value={university} onChange={(e)=> setUniversity(e.target.value)} type="text" className="upload-input"/>
+                        <br/>
+                        <br/>
+                        <label className="upload-labels" for="course">Course:</label>
+                        <br/>
+                        <input value={course} onChange={(e)=> setCourse(e.target.value)} type="text" className="upload-input"/>
+                    </div>
+
+                    <div className="flex-child">
+                        <label className="upload-labels" for="Upload">Choose Your File:</label>
+                        <br/>
+                        <div className="upload-button-container">
+                            <button className="button upload-button">Upload</button>
+                        </div>
+                        <br/>
+                        <label className="upload-labels" for="selected-file">Selected File:</label>
+                        <br/>
+                        <input value={selectedFile} onChange={(e)=> setSelectedFile(e.target.value)} type="text" className="upload-filename" disabled/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <button className="button" type="submit">Submit</button>
+                    </div>
+                    </div>
                 </form>
-                
-                
-        
             </div>
 
         </div>
